2e7005e6f94c4a17ac55938b09e7c762fca1c497
Caroline Yeh <caroline1020@gmail.com>
2021-05-03 12:05:12 +0800

    user list adapter

--- app/src/main/java/com/caroline/androidexercise/MainActivity.kt
+++ app/src/main/java/com/caroline/androidexercise/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.caroline.androidexercise
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.caroline.androidexercise.databinding.ActivityMainBinding
+import com.caroline.androidexercise.network.model.GitHubUser
+import com.caroline.androidexercise.userlist.OnItemClickListener
+import com.caroline.androidexercise.userlist.UsersAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private val adapter = UsersAdapter(object : OnItemClickListener {
+        override fun onItemClick(item: GitHubUser) {
+            TODO("Not yet implemented")
+        }
+    })
+
+    private fun initView() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/caroline/androidexercise/userlist/UsersAdapter.kt
@@ -0,0 +1,55 @@
+package com.caroline.androidexercise.userlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.caroline.androidexercise.databinding.ItemUserBinding
+import com.caroline.androidexercise.network.model.GitHubUser
+
+class UsersAdapter(private val onClickListener: OnItemClickListener) :
+    RecyclerView.Adapter<ViewHolder>() {
+    private val list: ArrayList<GitHubUser> = ArrayList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position], onClickListener)
+    }
+
+    fun setData(newList: ArrayList<GitHubUser>) {
+        list.clear()
+        list.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
+
+interface OnItemClickListener {
+    fun onItemClick(item: GitHubUser)
+}
+
+class ViewHolder private constructor(private val binding: ItemUserBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+
+    fun bind(item: GitHubUser, listener: OnItemClickListener) {
+        binding.item = item
+        binding.executePendingBindings()
+        itemView.setOnClickListener { _ ->
+            listener.onItemClick(item)
+        }
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = ItemUserBinding.inflate(layoutInflater, parent, false)
+            return ViewHolder(view)
+        }
+    }
+
+}

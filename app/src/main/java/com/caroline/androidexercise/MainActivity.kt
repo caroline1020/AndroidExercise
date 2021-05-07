b9e5aea2f492565ad06820aca705c8706cf6c6e4
Caroline Yeh <caroline1020@gmail.com>
2021-05-07 10:20:06 +0800

    extract UserListFragment

--- app/src/main/java/com/caroline/androidexercise/MainActivity.kt
+++ app/src/main/java/com/caroline/androidexercise/MainActivity.kt
@@ -1,77 +1,14 @@
 package com.caroline.androidexercise
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.caroline.androidexercise.databinding.ActivityMainBinding
-import com.caroline.androidexercise.detail.UserDetailActivity
-import com.caroline.androidexercise.network.model.GitHubUser
-import com.caroline.androidexercise.network.utils.HttpResult
-import com.caroline.androidexercise.userlist.OnItemClickListener
-import com.caroline.androidexercise.userlist.UsersAdapter
-import com.caroline.androidexercise.viewmodel.UserListViewModel
-import com.caroline.androidexercise.widgets.LoadMoreRecyclerViewAdapter
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    val viewModel: UserListViewModel by lazy {
-        ViewModelProvider(this).get(UserListViewModel::class.java)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initView()
-        registerObservers()
+        setContentView(R.layout.activity_main)
     }
 
-    private fun registerObservers() {
-
-        viewModel.result.observe(this, Observer {
-            when (it) {
-                is HttpResult.Success -> {
-                    it.data.body()?.let { list ->
-                        val hasMoreData = viewModel.hasNextPage()
-                        usersAdapter.onLoadMoreEnd(list, hasMoreData)
-                    }
-                }
-                is HttpResult.ApiError -> {
-                    Toast.makeText(this, it.apiError, Toast.LENGTH_SHORT).show()
-                }
-                is HttpResult.HttpError -> {
-                    Toast.makeText(this, it.exception.localizedMessage, Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
-        viewModel.loading.observe(this, Observer {
-            binding.swipeRefreshLayout.isRefreshing = it == View.VISIBLE
-        })
-    }
-
-    private val usersAdapter = UsersAdapter(object : OnItemClickListener {
-        override fun onItemClick(item: GitHubUser) {
-            startActivity(UserDetailActivity.createIntent(this@MainActivity, item.username))
-        }
-    }, object : LoadMoreRecyclerViewAdapter.OnLoadMoreListener {
-        override fun onLoadMore() {
-            viewModel.loadMoreUsers()
-        }
-
-    })
-
-    private fun initView() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        binding.recyclerView.adapter = usersAdapter
-        binding.swipeRefreshLayout.setOnRefreshListener {
-            binding.swipeRefreshLayout.isRefreshing = false
-            viewModel.reset()
-            usersAdapter.reset()
-        }
-
-    }
 }
---
+++ app/src/main/java/com/caroline/androidexercise/userlist/UserListFragment.kt
@@ -0,0 +1,88 @@
+package com.caroline.androidexercise.userlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.caroline.androidexercise.R
+import com.caroline.androidexercise.databinding.FragmentUserListBinding
+import com.caroline.androidexercise.detail.UserDetailActivity
+import com.caroline.androidexercise.network.model.GitHubUser
+import com.caroline.androidexercise.network.utils.HttpResult
+import com.caroline.androidexercise.viewmodel.UserListViewModel
+import com.caroline.androidexercise.widgets.LoadMoreRecyclerViewAdapter
+
+/**
+ * Created by nini on 2021/5/7.
+ */
+class UserListFragment : Fragment() {
+
+    val viewModel: UserListViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(UserListViewModel::class.java)
+    }
+    lateinit var binding: FragmentUserListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_user_list, container, false
+        )
+        val view: View = binding.root
+        initViewBinding()
+        registerObservers()
+        return view
+    }
+
+    private fun registerObservers() {
+        viewModel.result.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is HttpResult.Success -> {
+                    it.data.body()?.let { list ->
+                        val hasMoreData = viewModel.hasNextPage()
+                        usersAdapter.onLoadMoreEnd(list, hasMoreData)
+                    }
+                }
+                is HttpResult.ApiError -> {
+                    Toast.makeText(context, it.apiError, Toast.LENGTH_SHORT).show()
+                }
+                is HttpResult.HttpError -> {
+                    Toast.makeText(context, it.exception.localizedMessage, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        })
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+            binding.swipeRefreshLayout.isRefreshing = it == View.VISIBLE
+        })
+    }
+
+    private val usersAdapter = UsersAdapter(object : OnItemClickListener {
+        override fun onItemClick(item: GitHubUser) {
+            startActivity(UserDetailActivity.createIntent(requireActivity(), item.username))
+        }
+    }, object : LoadMoreRecyclerViewAdapter.OnLoadMoreListener {
+        override fun onLoadMore() {
+            viewModel.loadMoreUsers()
+        }
+
+    })
+
+    private fun initViewBinding() {
+        binding.lifecycleOwner = this
+        binding.recyclerView.adapter = usersAdapter
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.swipeRefreshLayout.isRefreshing = false
+            viewModel.reset()
+            usersAdapter.reset()
+        }
+
+    }
+}
